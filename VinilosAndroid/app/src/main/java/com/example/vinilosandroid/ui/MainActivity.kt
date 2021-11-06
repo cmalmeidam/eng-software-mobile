b3e406eb139114dfa9632897bd703bda09d3c8c1
Andrea Cardenas <pa.cardenasg1@uniandes.edu.co>
2021-11-05 20:23:53 -0400

    se agrega nav no funciona.

--- VinilosAndroid/app/src/main/java/com/example/vinilosandroid/ui/MainActivity.kt
+++ VinilosAndroid/app/src/main/java/com/example/vinilosandroid/ui/MainActivity.kt
@@ -10,21 +10,29 @@ import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import android.view.Menu
 import android.view.MenuItem
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import com.example.vinilosandroid.R
 import com.example.vinilosandroid.databinding.ActivityMainBinding
+import android.view.View
+import androidx.fragment.app.FragmentTransaction
+
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 
 class MainActivity : AppCompatActivity() {
     private lateinit var navController: NavController
+    private val albumFragment = AlbumFragment()
+    private val musicianFragment = MusicianFragment()
+    private val collectorFragment = CollectorFragment()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
-        // Get the navigation host fragment from this Activity
+        replaceFragment(albumFragment)
         val navHostFragment = supportFragmentManager
             .findFragmentById(R.id.nav_host_fragment) as NavHostFragment
         // Instantiate the navController using the NavHostFragment
@@ -34,8 +42,24 @@ class MainActivity : AppCompatActivity() {
         setSupportActionBar(findViewById(R.id.my_toolbar))
         setupActionBarWithNavController(navController)
 
+        val bottomNavigationView =
+            findViewById<View>(R.id.bottomNav) as BottomNavigationView
+        bottomNavigationView.setOnNavigationItemSelectedListener {
+            when (it.itemId) {
+                R.id.action_album -> replaceFragment(albumFragment)
+                R.id.action_musician -> replaceFragment(musicianFragment)
+                R.id.action_collector -> replaceFragment(collectorFragment)
+            }
+            true
+        }
     }
-
+    private fun replaceFragment(fragment: Fragment){
+        if(fragment !=null){
+            val transaction = supportFragmentManager.beginTransaction()
+            transaction.replace(R.id.nav_host_fragment, fragment)
+            transaction.commit()
+        }
+    }
     override fun onSupportNavigateUp(): Boolean {
         return navController.navigateUp() || super.onSupportNavigateUp()
     }
