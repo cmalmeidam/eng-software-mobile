628976c42fb478b36b13c1f4937ac95c3acdaba2
Daniel Velasquez <d.velasquezo@uniandes.edu.co>
2021-11-20 16:22:09 -0500

    Update PE18InformacionDetalladaAlbum.kt

--- VinilosAndroid/app/src/androidTest/java/com/example/vinilosandroid/ui/PE18InformacionDetalladaAlbum.kt
+++ VinilosAndroid/app/src/androidTest/java/com/example/vinilosandroid/ui/PE18InformacionDetalladaAlbum.kt
@@ -1,22 +1,20 @@
 package com.example.vinilosandroid.ui
 
-
 import android.view.View
 import android.view.ViewGroup
-import androidx.recyclerview.widget.RecyclerView.ViewHolder
-import androidx.test.espresso.Espresso.onView
-import androidx.test.espresso.action.ViewActions.click
-import androidx.test.espresso.assertion.ViewAssertions.matches
-import androidx.test.espresso.contrib.RecyclerViewActions.actionOnItemAtPosition
-import androidx.test.espresso.matcher.ViewMatchers.*
+import android.widget.LinearLayout
+import androidx.cardview.widget.CardView
+import androidx.test.espresso.Espresso
+import androidx.test.espresso.action.ViewActions
+import androidx.test.espresso.assertion.ViewAssertions
+import androidx.test.espresso.matcher.ViewMatchers
 import androidx.test.filters.LargeTest
 import androidx.test.rule.ActivityTestRule
 import androidx.test.runner.AndroidJUnit4
 import com.example.vinilosandroid.R
 import org.hamcrest.Description
 import org.hamcrest.Matcher
-import org.hamcrest.Matchers.`is`
-import org.hamcrest.Matchers.allOf
+import org.hamcrest.Matchers
 import org.hamcrest.TypeSafeMatcher
 import org.hamcrest.core.IsInstanceOf
 import org.junit.Rule
@@ -25,135 +23,49 @@ import org.junit.runner.RunWith
 
 @LargeTest
 @RunWith(AndroidJUnit4::class)
-class PE18InformacionDetalladaAlbum {
+class PE18DetalleArtistaDescripcion {
 
     @Rule
     @JvmField
     var mActivityTestRule = ActivityTestRule(MainActivity::class.java)
 
     @Test
-    fun PE18InformacionDetalladaAlbum() {
+    fun pE18DetalleArtistaDescripcion() {
         Thread.sleep(1000)
-        val recyclerView = onView(
-            allOf(
-                withId(R.id.albums_rv),
+        val bottomNavigationItemView = Espresso.onView(
+            Matchers.allOf(
+                ViewMatchers.withId(R.id.musicianFragment),
+                ViewMatchers.withContentDescription("Artistas"),
                 childAtPosition(
-                    withClassName(`is`("androidx.constraintlayout.widget.ConstraintLayout")),
-                    1
-                )
-            )
-        )
-        recyclerView.perform(actionOnItemAtPosition<ViewHolder>(0, click()))
-
-        val textView = onView(
-            allOf(
-                withId(R.id.textName), withText("Buscando América"),
-                withParent(withParent(IsInstanceOf.instanceOf(android.widget.FrameLayout::class.java))),
-                isDisplayed()
-            )
-        )
-        textView.check(matches(withText("Buscando América")))
-
-        val textView2 = onView(
-            allOf(
-                withId(R.id.textdescription),
-                withText("Buscando América es el primer álbum de la banda de Rubén Blades y Seis del Solar lanzado en 1984. La producción, bajo el sello Elektra, fusiona diferentes ritmos musicales tales como la salsa, reggae, rock, y el jazz latino. El disco fue grabado en Eurosound Studios en Nueva York entre mayo y agosto de 1983."),
-                withParent(withParent(IsInstanceOf.instanceOf(android.widget.FrameLayout::class.java))),
-                isDisplayed()
-            )
-        )
-        textView2.check(matches(withText("Buscando América es el primer álbum de la banda de Rubén Blades y Seis del Solar lanzado en 1984. La producción, bajo el sello Elektra, fusiona diferentes ritmos musicales tales como la salsa, reggae, rock, y el jazz latino. El disco fue grabado en Eurosound Studios en Nueva York entre mayo y agosto de 1983.")))
-
-        val textView3 = onView(
-            allOf(
-                withId(R.id.textGeneroTitle), withText("Genero"),
-                withParent(withParent(IsInstanceOf.instanceOf(android.widget.TableLayout::class.java))),
-                isDisplayed()
-            )
-        )
-        textView3.check(matches(withText("Genero")))
-
-        val textView4 = onView(
-            allOf(
-                withId(R.id.textGeneroValue), withText("Salsa"),
-                withParent(withParent(IsInstanceOf.instanceOf(android.widget.TableLayout::class.java))),
-                isDisplayed()
-            )
-        )
-        textView4.check(matches(withText("Salsa")))
-
-        val textView5 = onView(
-            allOf(
-                withId(R.id.textFechaLanzTitle), withText("Fecha de Lanzamiento"),
-                withParent(withParent(IsInstanceOf.instanceOf(android.widget.TableLayout::class.java))),
-                isDisplayed()
-            )
-        )
-        textView5.check(matches(withText("Fecha de Lanzamiento")))
-
-        val textView6 = onView(
-            allOf(
-                withId(R.id.textFechaLanzValue), withText("01-08-1984"),
-                withParent(withParent(IsInstanceOf.instanceOf(android.widget.TableLayout::class.java))),
-                isDisplayed()
-            )
-        )
-        textView6.check(matches(withText("01-08-1984")))
-
-        val textView7 = onView(
-            allOf(
-                withId(R.id.textCasaDiscoTitle), withText("Casa discográfica"),
-                withParent(withParent(IsInstanceOf.instanceOf(android.widget.TableLayout::class.java))),
-                isDisplayed()
-            )
-        )
-        textView7.check(matches(withText("Casa discográfica")))
-
-        val textView8 = onView(
-            allOf(
-                withId(R.id.textCasaDiscoValue), withText("Elektra"),
-                withParent(withParent(IsInstanceOf.instanceOf(android.widget.TableLayout::class.java))),
-                isDisplayed()
-            )
-        )
-        textView8.check(matches(withText("Elektra")))
-
-        val appCompatImageButton = onView(
-            allOf(
-                withContentDescription("Navigate up"),
-                childAtPosition(
-                    allOf(
-                        withId(R.id.my_toolbar),
-                        childAtPosition(
-                            withClassName(`is`("androidx.constraintlayout.widget.ConstraintLayout")),
-                            0
-                        )
+                    childAtPosition(
+                        ViewMatchers.withId(R.id.bottomnav),
+                        0
                     ),
                     1
                 ),
-                isDisplayed()
+                ViewMatchers.isDisplayed()
             )
         )
-        appCompatImageButton.perform(click())
-
-        val textView9 = onView(
-            allOf(
-                withId(R.id.textView7), withText("Albumes para tí"),
-                withParent(withParent(IsInstanceOf.instanceOf(android.widget.ScrollView::class.java))),
-                isDisplayed()
+        bottomNavigationItemView.perform(ViewActions.click())
+        Thread.sleep(1000)
+        val textView2 = Espresso.onView(
+            Matchers.allOf(
+                ViewMatchers.withId(R.id.textView10),
+                ViewMatchers.withText("Rubén Blades Bellido de Luna"),
+                ViewMatchers.withParent(ViewMatchers.withParent(IsInstanceOf.instanceOf(CardView::class.java))),
+                ViewMatchers.isDisplayed()
             )
         )
-        textView9.check(matches(withText("Albumes para tí")))
-
-        val textView10 = onView(
-            allOf(
-                withId(R.id.textView5), withText("Buscando América"),
-                withParent(withParent(IsInstanceOf.instanceOf(androidx.cardview.widget.CardView::class.java))),
-                isDisplayed()
+        textView2.perform(ViewActions.click())
+        val textView3 = Espresso.onView(
+            Matchers.allOf(
+                ViewMatchers.withId(R.id.textView12),
+                ViewMatchers.withParent(IsInstanceOf.instanceOf(LinearLayout::class.java)),
+                ViewMatchers.isDisplayed()
             )
         )
-        textView10.check(matches(withText("Buscando América")))
-
+        textView3.check(ViewAssertions.matches(ViewMatchers.withText(
+            "Es un cantante, compositor, músico, actor, abogado, político y activista panameño. Ha desarrollado gran parte de su carrera artística en la ciudad de Nueva York.")))
     }
 
     private fun childAtPosition(
