21a90be3e1b434fe79bb5a5be0357f3a3df8b741
Jonathan Verdugo <ja.verdugo1473@uniandes.edu.co>
2021-11-28 18:47:56 -0500

    Ajustes y optimizaciones detalle coleccionista

--- VinilosAndroid/app/src/main/java/com/example/vinilosandroid/ui/CollectorDetailFragment.kt
+++ VinilosAndroid/app/src/main/java/com/example/vinilosandroid/ui/CollectorDetailFragment.kt
@@ -5,7 +5,6 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.navArgs
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.engine.DiskCacheStrategy
@@ -13,12 +12,10 @@ import com.bumptech.glide.request.RequestOptions
 import com.example.vinilosandroid.R
 import com.example.vinilosandroid.databinding.FragmentCollectorDetailBinding
 import com.example.vinilosandroid.models.Collector
-import com.example.vinilosandroid.viewmodels.CollectorViewModel
 
 class CollectorDetailFragment : Fragment() {
 
     private lateinit var binding: FragmentCollectorDetailBinding
-    private lateinit var viewModel: CollectorViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -36,14 +33,10 @@ class CollectorDetailFragment : Fragment() {
         }
         val args: CollectorDetailFragmentArgs by navArgs()
         activity.actionBar?.title = getString(R.string.coleccionistas)
-        viewModel = ViewModelProvider(this, CollectorViewModel.Factory(activity.application)).get(
-            CollectorViewModel::class.java)
         binding.collector = Collector(
             args.collectorId, args.name, args.telephone, args.email)
-        var tmp = getString(R.string.coleccionista_placeholder)
-        print(tmp)
         Glide.with(activity)
-            .load(tmp)
+            .load(getString(R.string.coleccionista_placeholder))
             .apply(
                 RequestOptions().diskCacheStrategy(DiskCacheStrategy.RESOURCE)
                     .placeholder(R.drawable.ic_collector)
