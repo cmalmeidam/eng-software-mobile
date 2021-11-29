0df2acceba8fbdf9f54d5f83a0db88eba1c1815b
Christian Almeida <c.almeidam@uniandes.edu.com>
2021-11-28 22:12:44 -0500

    Se cargan pruebas de HU

---
+++ VinilosAndroid/app/src/androidTest/java/com/example/vinilosandroid/ui/PE53DetalleColeccionistaAvatar.kt
@@ -0,0 +1,101 @@
+package com.example.vinilosandroid.ui
+
+import android.view.View
+import android.view.ViewGroup
+import android.widget.LinearLayout
+import androidx.recyclerview.widget.RecyclerView
+import androidx.test.espresso.Espresso
+import androidx.test.espresso.action.ViewActions
+import androidx.test.espresso.assertion.ViewAssertions
+import androidx.test.espresso.contrib.RecyclerViewActions
+import androidx.test.espresso.matcher.ViewMatchers
+import androidx.test.filters.LargeTest
+import androidx.test.rule.ActivityTestRule
+import androidx.test.runner.AndroidJUnit4
+import com.example.vinilosandroid.R
+import org.hamcrest.Description
+import org.hamcrest.Matcher
+import org.hamcrest.Matchers
+import org.hamcrest.TypeSafeMatcher
+import org.hamcrest.core.IsInstanceOf
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@LargeTest
+@RunWith(AndroidJUnit4::class)
+class PE53DetalleColeccionistaAvatar {
+
+    @Rule
+    @JvmField
+    var mActivityTestRule = ActivityTestRule(MainActivity::class.java)
+
+    @Test
+    fun pE52DetalleColeccionistaNombre() {
+        Thread.sleep(1000)
+        val bottomNavigationItemView = Espresso.onView(
+            Matchers.allOf(
+                ViewMatchers.withId(R.id.collectorFragment),
+                ViewMatchers.withContentDescription("Coleccionistas"),
+                childAtPosition(
+                    childAtPosition(
+                        ViewMatchers.withId(R.id.bottomnav),
+                        0
+                    ),
+                    2
+                ),
+                ViewMatchers.isDisplayed()
+            )
+        )
+        bottomNavigationItemView.perform(ViewActions.click())
+        Thread.sleep(1000)
+        val textView = Espresso.onView(
+            Matchers.allOf(
+                ViewMatchers.withId(R.id.textView6), ViewMatchers.withText("Coleccionistas"),
+                ViewMatchers.withParent(ViewMatchers.withParent(ViewMatchers.withId(R.id.nav_host_fragment))),
+                ViewMatchers.isDisplayed()
+            )
+        )
+        textView.check(ViewAssertions.matches(ViewMatchers.withText("Coleccionistas")))
+
+        val recyclerView = Espresso.onView(
+            Matchers.allOf(
+                ViewMatchers.withId(R.id.fragments_rv),
+                childAtPosition(
+                    ViewMatchers.withClassName(Matchers.`is`("androidx.constraintlayout.widget.ConstraintLayout")),
+                    1
+                )
+            )
+        )
+        recyclerView.perform(RecyclerViewActions.actionOnItemAtPosition<RecyclerView.ViewHolder>(0, ViewActions.click()))
+        Thread.sleep(1000)
+        val imageView = Espresso.onView(
+            Matchers.allOf(
+                ViewMatchers.withId(R.id.item_avatar_iv),
+                ViewMatchers.withParent(IsInstanceOf.instanceOf(LinearLayout::class.java)),
+                ViewMatchers.isDisplayed()
+            )
+        )
+        imageView.check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+        Thread.sleep(1000)
+    }
+
+    private fun childAtPosition(
+        parentMatcher: Matcher<View>, position: Int
+    ): Matcher<View> {
+
+        return object : TypeSafeMatcher<View>() {
+            override fun describeTo(description: Description) {
+                description.appendText("Child at position $position in parent ")
+                parentMatcher.describeTo(description)
+            }
+
+            public override fun matchesSafely(view: View): Boolean {
+                val parent = view.parent
+                return parent is ViewGroup && parentMatcher.matches(parent)
+                        && view == parent.getChildAt(position)
+            }
+        }
+    }
+
+}
